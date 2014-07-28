aa38ae40f71fc0dc36b6e6859218e0d63aa30b8b
Prelang Builder <builder@prelang.com>
2014-07-28 08:11:43 +0000

    Generated model Exercise with migration

---
+++ db/migrate/20140728081143_create_exercises.rb
@@ -0,0 +1,15 @@
+class CreateExercises < ActiveRecord::Migration
+  def change
+    create_table :exercises do |t|
+      t.string :exercise_performed
+      t.integer :load_prescribed
+      t.integer :sets_perscribed
+      t.integer :reps_perscribed
+      t.integer :set_one_actual
+      t.integer :set_two_actual
+      t.integer :set_three_actual
+
+      t.timestamps
+    end
+  end
+end
